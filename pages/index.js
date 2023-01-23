35bb8dd87723dda0c7fe443d61c3753cf2e9fb82
LeDjinn <timour.spiridonov@gmail.com>
2023-01-23 14:16:39 +0100

    instagram connected to Tim

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,62 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {useState, useEffect} from 'react'
 
 export default function Home() {
+  const [tokken, setTokken] = useState('');
+
+  
+  const handleInputChange = (event) => {
+    setTokken(event.target.value);
+  };
+  
+  const triggerLog=()=>{
+    console.log(
+      'tokken :', tokken, 'start date:', date.start,'end date:', date.end,'data:', data
+    )
+  }
+
+  const [data, setData] = useState('');
+
+  const [selectedValue, setSelectedValue] = useState('impressions');
+  const [date, setDate] = useState({
+     'start': '',
+     'end': ''
+  });
+
+
+  const handleDateStart = (e) => {
+    setDate({...date,
+      'start': e.target.value
+  });
+};
+
+const handleDateEnd = (e) => {
+  setDate({...date,
+    'end': e.target.value
+});
+};
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch('https://graph.facebook.com/v15.0/17841412246964887/insights?metric='+selectedValue+'&period=week&since='+date.start+'&until='+date.end+'&access_token='+tokken);
+        const data = await res.json();
+        setData(data.data[0].values);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchData();
+  }, [tokken, selectedValue,date]);
+
+
+  function handleChange(event) {
+    setSelectedValue(event.target.value);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +68,29 @@ export default function Home() {
       
      <div>
       <h1>Dashboard for social media </h1>
+      <div>
+          <input type="password" value={tokken} onChange={handleInputChange} />
+          <select value={selectedValue} onChange={handleChange}>
+              <option value="impressions">impressions</option>
+              <option value="reach">Reach</option>
+        </select>
+         
+         <div>   
+              <input type="date"onChange={handleDateStart}/>
+              <input type="date"onChange={handleDateEnd}/>
+              <p>Selected Date: {date.start}</p>
+              <p>Selected Date: {date.end}</p>    
+        </div>
+        <button onClick={triggerLog}>Test on console Log</button>
+    </div>
+ 
+     <p></p>
+     <div> Engagement/impresions --{data ? <>
+      {data.map(({ end_time, value }) => (
+        <p key={end_time}>On <span className='green-text bold-text'>{end_time.substring(0, end_time.length - 14)}</span> Tim had <span className='blue-text bold-text'>{value}</span> engagement/impressions.</p>
+      ))}
+    </>: <h2>tokken expired </h2> }</div>
+
       <ul>
         <li>Instagram</li>
         <li>Twitter</li>
